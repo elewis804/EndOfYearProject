16439fc578f2b6186737e309f2b7c70af2e8327d
elewis804 <60186516+elewis804@users.noreply.github.com>
2020-06-09 09:58:33 -0400

    Just removed the update function of playcardbutton

---
+++ Assets/Scripts/DrawCardScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCardScript : MonoBehaviour {
+
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onClick(){
+        GameObject playerCard = Instantiate(Card1, new Vector3(0,0,0), Quaternion.identity);
+        playerCard.transform.SetParent(PlayerArea.transform, false);
+    }
+}
